1709e663e967655489c3f9c01ff734294356aa99
PeterEmad1 <peteremad132oo5@gmail.com>
2026-03-14 09:54:55 +0200

    Refactors MDX component registration
    Updates how MDX components are registered by introducing a `useMDXComponents` hook. This change removes the previous hardcoded custom component mappings, promoting a more flexible approach where base components can be extended or overridden as needed.

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,8 +1,7 @@
 import type { MDXComponents } from "mdx/types";
-import CodeBlock from "@/components/mdx/CodeBlock";
-import Callout from "@/components/mdx/Callout";
 
-export const mdxComponents: MDXComponents = {
-  pre: CodeBlock,
-  Callout,
-};
+export function useMDXComponents(components: MDXComponents): MDXComponents {
+  return {
+    ...components,
+  };
+}
